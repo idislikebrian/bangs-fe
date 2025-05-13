aa88bc764bde12bd381077d947439d968b7cf173
Brian Felix <fbrian100@gmail.com>
2025-05-13 15:39:37 -0400

    Reactivate modal

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -41,7 +41,7 @@ const Project: React.FC<ProjectProps> = ({ video, onClose }) => {
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <iframe
           ref={iframeRef}
-          // src={`https://player.vimeo.com/video/${video.id}?autoplay=1&muted=0&title=0&byline=0&portrait=0&transparent=1&loop=0&api=1&player_id=vimeo-player`}
+          src={`https://player.vimeo.com/video/${video.id}?autoplay=1&muted=0&title=0&byline=0&portrait=0&transparent=1&loop=0&api=1&player_id=vimeo-player`}
           frameBorder="0"
           allow="autoplay; fullscreen"
           allowFullScreen
